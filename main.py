de4732ff4f07282a31729a6452be0866f777850c
haibo_at_github <wuhaibo@hotmail.com>
2021-07-08 14:41:17 +0200

    docker example

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+
+filename = "/var/pythonApp/logs/log.txt"
+file = open(filename, 'w+') 
+
+while True:
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    file.write(f"{current_time}: write action")
+    time.sleep(10)
